eec230b35b39cd80ed8fcfd71f0c88597fdcfde3
SchuylerM <schuyler.manchester@gmail.com>
2014-01-11 11:20:58 -0800

    Initial class and gitignore to keep our project pretty.?

---
+++ LearndingSomeJava/src/DataStructuresIntro.java
@@ -0,0 +1,8 @@
+
+public class DataStructuresIntro {
+
+	public static void main(String[] args) {
+		System.out.println("For now we will focus on utilizing Java data structures inside this one main class.");
+	}
+
+}
