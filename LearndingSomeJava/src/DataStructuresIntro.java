a8b763fd7a7cfe052810c817f3ee9bf90aeb85a3
SchuylerM <schuyler.manchester@gmail.com>
2014-01-11 17:44:35 -0800

    Prepping file for assignment to Austin.

--- LearndingSomeJava/src/DataStructuresIntro.java
+++ LearndingSomeJava/src/DataStructuresIntro.java
@@ -1,8 +1,30 @@
-
 public class DataStructuresIntro {
-
+	
+	/**
+	 * TODO: Output a comparison of the contains method for both a HashSet and an ArrayList
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("For now we will focus on utilizing Java data structures inside this one main class.");
+	
+	}
+	
+	/**
+	 * While timing the entire method, insert 100000 random numbers into a HashSet, and then perform 100000 random contains() calls
+	 * 
+	 * @return the average time (in seconds) for a .contains(x) call on a HashSet
+	 */
+	public static double hashSetMetrics() {
+		return 0.0;
+	}
+
+	/**
+	 * While timing the entire method, insert 100000 random numbers into an ArrayList, and then perform 100000 random contains() calls
+	 * 
+	 * @return the average time (in seconds) for a .contains(x) call on an ArrayList
+	 */
+	public static double arrayListMetrics() {
+		return 0.0;
 	}
 
 }
