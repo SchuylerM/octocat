ee43e09b82cd160b30a77bb7fe94637820431e4b
SchuylerM <schuyler.manchester@gmail.com>
2014-01-13 09:05:35 -0800

    Scrubs quotes console assignment.

---
+++ LearndingSomeJava/src/ScrubsQuotesConsole.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ScrubsQuotesConsole {
+
+	/**
+	 * Source of quotes: http://www.imdb.com/title/tt0285403/quotes
+	 */
+	private static List<String> scrubsQuotes = new ArrayList<String>(
+			Arrays.asList(
+					"Dr. Cox: [in response to something J.D. just said] Oh, my God! I care so little, I almost passed out!",
+					"Carla: [about a male intern] You're right; he definitely has a cute little butt.\nElliot: It's almost like it's been sculpted.\nJ.D.: Who cares? Everyone has a cute butt; I have a cute butt.\nCarla: You should bring it in someday.",
+					"Turk: How can you make love to your wife if she's at fat camp?\nDr. Kelso: The real question is: how can I make love to her when she's *not* at fat camp?",
+					"Carla: Christopher!\nChris Turk: Christopher? You only call me Christopher when you're mad or when we're having sex... Baby, are you mad when we're having sex?\nCarla: Sometimes.",
+					"[Somebody streaked in the hospital]\nChris Turk: I know it wasn't you last night. Look I'm not proud of this but I can pick your puff and stuff out of a line-up\nJ.D.: He changed since the last time you saw him. He got a haircut.",
+					"Dr. Cox: [on the phone] Sure, Jordan, you can take over the master bathroom. Just make sure you leave my sleeping pills out. So that I could, you know, swallow about 300 of them...",
+					"Janitor: Hey, Idiot.\n[J.D. turns around]\nJanitor: Heh. I said idiot and you turned around.",
+					"Todd: [Todd is playing Pacman] Oh, Miss Pacman, I would sex that bow right off your head. Eat those dots you naughty, naughty girl.",
+					"Jordan: I don't dislike you, I nothing you.\nJ.D.: That's special.",
+					"J.D.: [after Janitor pops up behind him] Why do you have to jump out and scare me all the time?\nJanitor: I don't jump out and scare you. I follow you around all day. I only got about an hour and a half of work around here, and the rest of the time I track you, like an animal."
+					));
+	
+	/**
+	 * TODO:
+	 * ****
+	 *  Write a program that interfaces with the console, and as you write any text it gives you a random scrubs quote
+	 *  the program should only give a quote if it receives text (if it's given blank text, it should say 'you must talk in order to get a scrubs quote.')
+	 *  the program will exit when the user types 'go away'.  Bonus points if it is case insensitive (GO AWAY and Go Away would make the program exit)
+	 *  
+	 *  To save you time, I have given you an ArrayList of scrubs quotes that you can use.
+	 * ****
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
