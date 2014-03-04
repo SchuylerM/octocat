ddb6743f7ee21a3c616fdd8b68694047af13d919
SchuylerM <schuyler.manchester@gmail.com>
2014-03-03 21:59:45 -0800

    ArrayProblems assignment.

---
+++ LearndingSomeJava/src/ArrayProblems.java
@@ -0,0 +1,17 @@
+
+public class ArrayProblems {
+
+	/**
+	 * TODO: Solve the following array problems:
+	 * 
+	 * 	1) write a method that accepts an array of characters (char[]) and returns true if the array is all unique characters, otherwise false
+	 *  2) write a method that accepts an array of characters (char[]) and returns an array of unique characters.
+	 *  3) write a method that accepts an array of characters (char[]) and returns the reversed order of that list ([a,b,c,d] -> [d,c,b,a]) 
+	 * 
+	 * Caution, if you look for help on the web of these problems you may stumble upon the solutions which will take the fun out of it, and you won't learn as much.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+}
